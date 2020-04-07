71789593411eaa3d19a1f8c215ff030387be487d
esandoval <xsandoval21x@gmail.com>
2020-04-06 21:45:39 -0700

    Create Calculator class to manipulating variables more manageable and got the calculator functioning

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+class Calculator {
+    constructor(previousTextElement, currentTextElement) {
+        this.previousTextElement = previousTextElement;
+        this.currentTextElement = currentTextElement;
+        this.clear();
+
+    }
+
+    clear() {
+        this.current = '';
+        this.previous = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.current = this.current.toString().slice(0, -1);
+    }
+
+    insertNum(num) {
+        if(num === '.' && this.current.includes('.')) return;
+        this.current += num;
+    }
+
+    insertOperator(operation) {
+        if(this.current === '') return;
+        if(this.previous !== ''){
+            this.compute();
+        }
+        this.operation = operation;
+        this.previous = this.current;
+        this.current = '';
+    }
+
+    compute() {
+        let total;
+        const prev = parseFloat(this.previous);
+        const curr = parseFloat(this.current);
+
+        if(isNaN(prev) || isNaN(curr)) return;
+
+        switch(this.operation){
+            case '+':
+                total = prev + curr;
+                break;
+            case '-':
+                total = prev - curr;
+                break;
+            case '*':
+                total = prev * curr;
+                break;
+            case '/':
+                total = prev / curr;
+                break;
+        }
+
+        this.current = total;
+        this.operation = undefined;
+        this.previous = '';
+    }
+
+    getDisplayNumber(num) {
+        const stringNum = num.toString();
+        const intDigits = parseFloat(stringNum.split('.')[0]);
+        const decimalDigits = stringNum.split('.')[1];
+        let integerDisplay;
+
+        if(isNaN(intDigits)){
+            integerDisplay = '';
+        }else {
+            integerDisplay = intDigits.toLocaleString('en',{
+                maximumFractionDigits: 0
+            });
+        }
+
+        if(decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        }else {
+            return integerDisplay;
+        }
+    }
+
+    updateDisplay() {      
+        this.currentTextElement.innerText = this.getDisplayNumber(this.current);
+        if(this.operation != null) {
+            this.previousTextElement.innerText = `${this.getDisplayNumber(this.previous)} ${this.operation}`;
+        }else {
+            this.previousTextElement.innerText = this.previous;
+        }
+        
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const clearButton = document.querySelector('button[id=clear]');
@@ -6,103 +98,37 @@ const deleteButton = document.querySelector('button[id=delete]');
 const previousTextElement = document.querySelector('div[id=previous-input]');
 const currentTextElement = document.querySelector('div[id=current-input]');
 
+const calculator = new Calculator(previousTextElement, currentTextElement);
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        insertNum(button.innerText);
+        calculator.insertNum(button.innerText);
+        calculator.updateDisplay();
     })
 });
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        insertOperator(button.innerText);
+        calculator.insertOperator(button.innerText);
+        calculator.updateDisplay();
     })
 });
 
-equalButton.addEventListener('click', equate);
+equalButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
 
-let total = 0;
-let operator;
-let a = 0;
-let b = 0;
+clearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
 
-function insertNum(num) {
-    if(num === '.' && currentTextElement.innerText.includes('.')) return;
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
 
-    const tempInput = currentTextElement.textContent;
-    document.getElementById('current-input').innerText = tempInput + num;
-}
-
-function insertOperator(op) {
-    const tempInput = currentTextElement.textContent;
-    if(a == 0){
-        if(total == 0){
-            a = tempInput; 
-        }else{
-            a = total;
-        }
-    }else{
-        b = tempInput.substring(tempInput.indexOf(operator)+1);
-        operate(operator, a, b);    
-    }
-    operator = op;
-    
-    if(total == 0){
-        document.getElementById('previous-input').textContent = tempInput + op;
-    }else {
-        document.getElementById('previous-input').textContent = total + op;
-    }
-    document.getElementById('current-input').textContent = '';
-}
-
-document.getElementById('clear').addEventListener('click', clear);
-
-function clear() {
-    document.getElementById('previous-input').textContent = '';
-    document.getElementById('current-input').textContent = '';
-    total = 0;
-    a = 0;
-    b = 0;
-}
-
-function equate() {
-    const tempInput = currentTextElement.textContent;
-    b = tempInput.substring(tempInput.indexOf(operator)+1);
-    operate(operator, a, b);
-    document.getElementById('current-input').textContent = `${total}`;
-    document.getElementById('previous-input').textContent = '';
-}
-
-function add(a, b) {
-    return Number(a) + Number(b);
-}
-
-function subtract(a, b) {
-    return Number(a) - Number(b);
-}
-
-function multiply(a, b) {
-    return Number(a) * Number(b);
-}
-
-function divide(a, b) {
-    return Number(a) / Number(b);
-}
-
-function operate(operator, a, b){
-    switch(operator){
-        case '+':
-            total = add(a, b);
-            break;
-        case '-':
-            total = subtract(a, b);
-            break;
-        case '*':
-            total = multiply(a, b);
-            break;
-        case '/':
-            total = divide(a, b);
-            break;
-    }
-
-    a = 0;
-}
+operationButtons.forEach(button => {
+    button.classList.add('operator-buttons');
+});
